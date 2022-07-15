e82ac98c76b72205c485535b390cc77b3e896efc
Abel <90abel.avr@gmail.com>
2022-07-15 00:42:56 -0300

    second commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,33 @@
+import CartWidget from "../cartWidget/CartWidget";
+
+const NavBar = () => {
+    console.log('es este');
+  return (
+    <>
+      <header className="header">
+            <div className="header__barra">
+                <div className="header__navegacion">
+                    <a href="##" className="header__link">
+                        Inicio
+                    </a>
+                    <a href="##" className="header__link">
+                        Productos
+                    </a>
+                    <a href="##" className="header__link">
+                        Nosotros
+                    </a>
+                    <a href="##" className="header__link">
+                        Contancto
+                    </a>
+                    <a href="##" className="header__link carrito">
+                        <CartWidget />
+                    </a>
+                </div>
+
+            </div> 
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ src/components/cartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+import carrito from '../../imagenes/bx-cart.svg' ;
+
+const CartWidget = () => {
+  return (
+    <>
+     <img src={carrito} alt="carrito" />    
+    </>
+  )
+}
+
+export default CartWidget
